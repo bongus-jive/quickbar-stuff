ea3985ee6e0dcf457a430b3a455de273547d0002
patmanf <27874300+patmanf@users.noreply.github.com>
2021-09-19 00:40:00 -0500

    block cheese autodismiss, imrpove cheesing, add quickbar/stardustlib to includes (oops)

--- quickbar/pat/cheese.lua
+++ quickbar/pat/cheese.lua
@@ -1,11 +1,15 @@
-local swap = player.swapSlotItem()
-local itemId = params and params[1] or "perfectlygenericitem"
+do
+	local swap = player.swapSlotItem()
+	local itemId = params and params[1] or "perfectlygenericitem"
 
-if swap == nil then
-	player.setSwapSlotItem({name = itemId})
-elseif swap.name == itemId then
-	swap.count = (swap.count or 1) + 1
-	player.setSwapSlotItem(swap)
-else
-	pane.playSound("/sfx/interface/clickon_error.ogg")
+	sb.logInfo("%s", itemDesc)
+
+	if swap == nil then
+		player.setSwapSlotItem(itemId)
+	elseif root.itemDescriptorsMatch(swap, itemId) then
+		swap.count = (swap.count or 1) + 1
+		player.setSwapSlotItem(swap)
+	else
+		pane.playSound("/sfx/interface/clickon_error.ogg")
+	end
 end
