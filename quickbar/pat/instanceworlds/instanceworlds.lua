ea057c30357b3c2c8aeec362eba02652b99e2489
patmanf <27874300+patmanf@users.noreply.github.com>
2023-04-14 13:26:00 -0500

    more instanceworld icons for mods (mcdonalds, elithian, shellguard, starforge, knightfall)

--- quickbar/pat/instanceworlds/instanceworlds.lua
+++ quickbar/pat/instanceworlds/instanceworlds.lua
@@ -1,28 +1,33 @@
+do
+
 local cfg = root.assetJson("/quickbar/pat/instanceworlds/instanceworlds.config")
 local worlds = root.assetJson("/instance_worlds.config")
 local destinations = {}
 
+local priorityIcons = copy(cfg.icons)
+for i,v in ipairs(priorityIcons) do
+	if not v.priority then v.priority = tonumber("0.0000"..i) end
+end
+table.sort(priorityIcons, function(a,b) return (a.priority or 0) < (b.priority or 0) end)
+
 local function findIcon(name)
-	for _,v in ipairs(cfg.icons) do
-		--match exact names
-		if v.match then
-			for _,s in ipairs(v.match) do
+	for _,v in ipairs(priorityIcons) do
+		if v.names then
+			for _,s in ipairs(v.names) do
 				if name == s then
 					return v.icon
 				end
 			end
 		end
 		
-		--find strings
-		if v.find then
-			for _,s in pairs(v.find) do
-				if name:find(s) then
+		if v.patterns then
+			for _,s in ipairs(v.patterns) do
+				if name:lower():match(s) then
 					return v.icon
 				end
 			end
 		end
 	end
-	
 	return "default"
 end
 
@@ -32,6 +37,7 @@ for k,_ in pairs(worlds) do
 	table.insert(list, k)
 end
 table.sort(list)
+
 
 --find icon and add to destinations
 for _,name in ipairs(list) do
@@ -71,3 +77,5 @@ player.interact("OpenTeleportDialog", {
 	includePlayerBookmarks = false,
 	destinations = sortedDestinations
 }, player.id())
+
+end
