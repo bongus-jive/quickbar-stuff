83953d1cb004bd96d8f9d2c58fc881ee5dabad6e
patmanf <27874300+patmanf@users.noreply.github.com>
2026-03-20 19:13:52 -0500

    rewrite teleporter and instanceworlds

--- quickbar/pat/instanceworlds/instanceworlds.lua
+++ quickbar/pat/instanceworlds/instanceworlds.lua
@@ -1,81 +1,60 @@
-do
-
-local cfg = root.assetJson("/quickbar/pat/instanceworlds/instanceworlds.config")
+local icons = root.assetJson("/quickbar/pat/instanceworlds/instanceworlds.config:icons")
 local worlds = root.assetJson("/instance_worlds.config")
+
+local worldNames = {}
+for k, _ in pairs(worlds) do table.insert(worldNames, k) end
+table.sort(worldNames)
+
+for i, icon in ipairs(icons) do
+  icon.destinations = {}
+  icon.priority = icon.priority or 0
+  icon.sort = i
+end
+table.sort(icons, function(a, b) return (a.priority >= b.priority) and (a.sort < b.sort) end)
+
+local iconNames, iconPatterns = {}, {}
+for i = #icons, 1, -1 do
+  local icon = icons[i]
+  for _, name in ipairs(icon.names or {}) do iconNames[name] = icon end
+  for _, pattern in ipairs(icon.patterns or {}) do iconPatterns[pattern] = icon end
+end
+
+local iconDefault = { icon = "default", destinations = {} }
+table.insert(icons, iconDefault)
+
+local function findIcon(worldName)
+  local named = iconNames[worldName]
+  if named then return named end
+
+  for pattern, icon in pairs(iconPatterns) do
+    if worldName:lower():match(pattern) then return icon end
+  end
+
+  return iconDefault
+end
+
+for _, name in ipairs(worldNames) do
+  local world = worlds[name]
+  local icon = findIcon(name)
+
+  table.insert(icon.destinations, {
+    name = name,
+    warpAction = string.format("instanceworld:%s", name),
+    planetName = world.persistent and "^darkgray;persistent" or "",
+    icon = icon.icon
+  })
+end
+
 local destinations = {}
-
-local priorityIcons = copy(cfg.icons)
-for i,v in ipairs(priorityIcons) do
-	if not v.priority then v.priority = tonumber("0.0000"..i) end
-end
-table.sort(priorityIcons, function(a,b) return (a.priority or 0) < (b.priority or 0) end)
-
-local function findIcon(name)
-	for _,v in ipairs(priorityIcons) do
-		if v.names then
-			for _,s in ipairs(v.names) do
-				if name == s then
-					return v.icon
-				end
-			end
-		end
-		
-		if v.patterns then
-			for _,s in ipairs(v.patterns) do
-				if name:lower():match(s) then
-					return v.icon
-				end
-			end
-		end
-	end
-	return "default"
+for _, icon in ipairs(icons) do
+  for _, destination in ipairs(icon.destinations) do
+    table.insert(destinations, destination)
+  end
 end
 
---sort world names
-local list = {}
-for k,_ in pairs(worlds) do
-	table.insert(list, k)
-end
-table.sort(list)
-
-
---find icon and add to destinations
-for _,name in ipairs(list) do
-	local world = worlds[name]
-	local icon = findIcon(name)
-	
-	table.insert(destinations, {
-		name = name,
-		planetName = world.persistent and "^darkgray;persistent" or "",
-		icon = icon,
-		warpAction = "instanceworld:"..name
-	})
-end
-
---sort destinations by icon
-local sortedDestinations = {}
-for _,v in ipairs(cfg.icons) do
-	for i,d in ipairs(destinations) do
-		if d.icon == v.icon then
-			table.insert(sortedDestinations, d)
-			d.sorted = true
-		end
-	end
-end
-
---add unsorted destinations
-for _,d in ipairs(destinations) do
-	if not d.sorted then
-		table.insert(sortedDestinations, d)
-	end
-end
-
---the funny
 player.interact("OpenTeleportDialog", {
-	canBookmark = false,
-	includePartyMembers = false,
-	includePlayerBookmarks = false,
-	destinations = sortedDestinations
+  canBookmark = false,
+  includePartyMembers = false,
+  includePlayerBookmarks = false,
+  destinations = destinations
 }, player.id())
-
-end
--- quickbar/pat/teleporter/teleporter.lua
+++ quickbar/pat/teleporter/teleporter.lua
@@ -1,28 +1,17 @@
-do
-
 local cfg = root.assetJson("/quickbar/pat/teleporter/teleporter.config")
-local destinations = {}
 local onShip = player.worldId() == player.ownShipWorldId()
+local destinations = {}
 
-for _,d in ipairs(cfg.destinations) do
-	if (d.onShip == nil
-	or (d.onShip == true and onShip)
-	or (d.onShip == false and not onShip))
-	and (not d.deploy or player.canDeploy()) then
-		d.onShip = nil
-		if d.warpAction == "self" then
-			local pos = world.entityPosition(player.id())
-			d.warpAction = string.format("Nowhere=%s.%s", math.floor(pos[1]), math.floor(pos[2]))
-		end
-		table.insert(destinations, d)
-	end
+for _, d in ipairs(cfg.destinations) do
+  if (d.onShip == nil or d.onShip == onShip)
+  or (not d.deploy or player.canDeploy()) then
+    table.insert(destinations, d)
+  end
 end
 
 player.interact("OpenTeleportDialog", {
-	canBookmark = false,
-	includePartyMembers = true,
-	includePlayerBookmarks = true,
-	destinations = destinations
+  canBookmark = false,
+  includePartyMembers = true,
+  includePlayerBookmarks = true,
+  destinations = destinations
 }, player.id())
-
-end
